90a43fd792fac49cfde39a79112b18218e1d723f
s-yamauchi07 <hand1nhand306@gmail.com>
2025-07-12 11:38:28 +0900

    Prettierでコードフォーマット

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { FC } from 'react';
+import { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-const App:FC = () => {
+const App: FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
